20f727a873695baecff4394c087d907fc34f0c86
Laguna1 <lagunazoomarket@gmail.com>
2020-04-23 18:19:26 +0300

    update my_each_with_index

--- main.rb
+++ main.rb
@@ -1,27 +1,29 @@
 def my_each
   return to_enum unless block_given?
 
-    index = 0
-    while index < length
-        yield(self[index])
-        index += 1
-      end
+  index = 0
+  while index < length
+    yield(self[index])
+    index += 1
+  end
+  self
 end
 
 def my_each_with_index
   return to_enum unless block_given?
 
-    index = 0
-    while index < length
-        yield([index], index)
-        index += 1
-      end
+  index = 0
+  while index < length
+    yield(self[index], index)
+    index += 1
+  end
+  self
 end
 
 def my_select
   return to_enum unless block_given?
 
-    array = []
-    my_each { |i| array.push(i) if yield(i) }
-    array
+  array = []
+  my_each { |i| array.push(i) if yield(i) }
+  array
 end
